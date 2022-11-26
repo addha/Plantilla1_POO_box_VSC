bc673c745dca4363060796f50d14386cba89d31a
addha <85791287+addha@users.noreply.github.com>
2022-11-26 17:43:27 -0600

    Add files via upload

---
+++ box.js
@@ -0,0 +1,25 @@
+class Box
+  {
+  // Anexo comentario
+    constructor()
+    {
+
+    }
+
+    show()
+    {   
+      // codigo rect = imprimir
+      
+    }
+     //Creacion de Funcion para Posicion X
+    set_speed(v)
+    {
+      //algoritmo matemtico
+    }
+
+  //Creacion de Funcion para Posicion Y
+    //Creacion de Funcion fill = color con atributo de entrada
+      //Creacion de Funcion fill = color con atributo de entrada
+  }
+
+  
---
+++ sketch.js
@@ -0,0 +1,17 @@
+//Etapa 1 Creacion de Objetos Programacion POO
+
+
+
+
+function setup() 
+{
+  createCanvas(400, 400);
+ 
+}
+
+function draw() 
+{
+  background(220);
+}
+
+
